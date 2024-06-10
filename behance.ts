8c95aa50ad9d56c3c2c407e5f396f8ef9bfc296b
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-06-09 21:53:34 -0400

    0.0.35

--- behance.ts
+++ behance.ts
@@ -1,7 +1,4 @@
 import {BeValueAdded} from './be-value-added.js';
 export {BeValueAdded} from './be-value-added.js';
-import {def} from 'trans-render/lib/def.js';
 
 await BeValueAdded.bootUp();
-
-def('be-value-added', BeValueAdded);
