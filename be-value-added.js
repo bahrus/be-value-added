4643d8e6b6ad477b2d087f8b0794d8b51c2772c8
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-08-09 18:37:12 -0400

    0.0.50

--- be-value-added.js
+++ be-value-added.js
@@ -3,7 +3,7 @@ import { propInfo, rejected, resolved } from 'be-enhanced/cc.js';
 import { BE } from 'be-enhanced/BE.js';
 import { dispatchEvent } from 'trans-render/positractions/dispatchEvent.js';
 /** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
-/** @import {BVAActions, BVAAllProps, BVAP} from './types.d.ts' */;
+/** @import {BVAActions, BVAAllProps, BVAP} from './ts-refs/be-value-added/types' */;
 
 /**
  * @implements {BVAActions}
